f7fdcbb0e9733435f1085b187982da6639d170ec
Juan Luis <juanluis@dropstudio.mx>
2026-03-13 11:40:54 -0600

    Inicializar el backend de Laravel con andamiaje básico y dependencias.

---
+++ app/Http/Controllers/SupplierProductController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\supplier_product;
+use Illuminate\Http\Request;
+
+class SupplierProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(supplier_product $supplier_product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(supplier_product $supplier_product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, supplier_product $supplier_product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(supplier_product $supplier_product)
+    {
+        //
+    }
+}
