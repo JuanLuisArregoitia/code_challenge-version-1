d26c741a587dd51148eec77a60a24da44a183520
Juan Luis <juanluis@dropstudio.mx>
2026-03-25 12:58:50 -0600

    Feat: Implementar controlador de autenticación con métodos base.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        //
+    }
+
+    public function login(Request $request)
+    {
+        //
+    }
+
+    public function me(Request $request)
+    {
+        //
+    }
+
+    public function logout(Request $request)
+    {
+        //
+    }
+
+    public function forgotPassword(Request $request)
+    {
+        //
+    }
+
+    public function resetPassword(Request $request)
+    {
+        //
+    }
+
+    public function verifyEmail(Request $request, $id, $hash)
+    {
+        //
+    }
+
+    public function resendVerificationEmail(Request $request)
+    {
+        //
+    }
+}
