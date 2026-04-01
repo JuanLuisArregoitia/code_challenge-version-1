6f1b83d671ef992d36806f4ef87c907466f0bb18
Juan Luis <juanluis@dropstudio.mx>
2026-04-01 17:25:40 -0600

    chore: Mejorar seeder de usuarios de la base de datos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,25 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $password = Hash::make('secret');
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        for ($i = 1; $i <= 10; $i++) {
+            $number = str_pad($i, 3, '0', STR_PAD_LEFT);
+
+            User::factory()->create([
+                'name'              => "Test User {$number}",
+                'email'             => "test{$number}@test.com",
+                'password'          => $password,
+                'email_verified_at' => now(),
+            ]);
+        }
     }
 }
