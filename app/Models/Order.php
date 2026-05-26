00e6d141e4c8d58b00e0baa6335e05caaff4f351
Juan Luis <juanluis@dropstudio.mx>
2026-05-26 12:55:59 -0600

    feat: Configurar modelos Eloquent con propiedades fillable, Soft Deletes y relaciones.

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'order_number',
+        'status_id',
+        'client_id',
+    ];
+
+   public function client(): belongsTo
+   {
+       return $this->belongsTo(Client::class);
+   }
 }
