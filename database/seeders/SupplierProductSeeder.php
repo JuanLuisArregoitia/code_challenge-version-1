ffb4413fa46c5908b3f3be3747bb7aa0e50a19cb
Juan Luis <juanluis@dropstudio.mx>
2026-05-28 16:37:08 -0600

    feat: Implementar SupplierProductSeeder para relaciones Many-to-Many

--- database/seeders/SupplierProductSeeder.php
+++ database/seeders/SupplierProductSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\Supplier;
 
 class SupplierProductSeeder extends Seeder
 {
@@ -12,6 +14,12 @@ class SupplierProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Product::all()->each(function (Product $product) {
+            $supplierIds = Supplier::inRandomOrder()
+                ->limit(fake()->numberBetween(1, 3))
+                ->pluck('id');
+
+            $product->suppliers()->attach($supplierIds);
+        });
     }
 }
