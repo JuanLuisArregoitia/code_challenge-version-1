94950c1ed8f9e03de8cd5f7a60ed92be0bfad207
Juan Luis <juanluis@dropstudio.mx>
2026-05-28 16:37:08 -0600

    style: Ajustar indentación en ClientSeeder

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -14,7 +14,7 @@ class ClientSeeder extends Seeder
     public function run(): void
     {
         Client::factory()
-        ->count(10)
-        ->create();
+            ->count(10)
+            ->create();
     }
 }
