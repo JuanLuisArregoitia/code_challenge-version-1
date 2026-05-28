e80538b3a45ba98100e3d748fdf3e7e9b24dd523
Juan Luis <juanluis@dropstudio.mx>
2026-05-28 11:20:43 -0600

    refactor: Añadir tipo de retorno a relación 'Client->orders()'

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
@@ -15,7 +16,8 @@ class Client extends Model
         'email',
     ];
 
-     public function orders() {
+     public function orders(): HasMany
+     {
         return $this->hasMany(Order::class);
     }
 }
