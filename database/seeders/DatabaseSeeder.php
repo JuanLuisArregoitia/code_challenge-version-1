0b95a213aca7b57f95b6bf8d2a47d7a4dea110d6
Juan Luis <juanluis@dropstudio.mx>
2026-05-29 11:45:49 -0600

    config: Registra OrderDetailSeeder en DatabaseSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             ProductSeeder::class,
             SupplierProductSeeder::class,
             OrderSeeder::class,
+            orderDetailSeeder::class,
         ]);
     }
 }
