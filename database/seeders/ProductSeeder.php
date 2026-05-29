e576994eef317ef16a5f447a1465bb63a582a8c2
Juan Luis <juanluis@dropstudio.mx>
2026-05-29 13:18:13 -0600

    fix: Corregir namespaces de seeders

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use app\Models\Product;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use app\Models\Supplier;
+use App\Models\Supplier;
 
 class SupplierSeeder extends Seeder
 {
